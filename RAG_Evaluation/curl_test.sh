0a5c854585b659a9ca273a0e68c6cf559d2da75e
Minji Choi <cmj04250@gmail.com>
2025-09-16 17:39:36 +0900

    .

--- RAG_Evaluation/curl_test.sh
+++ RAG_Evaluation/curl_test.sh
@@ -1,6 +1,3 @@
-
-SESSION_ID="22cb3a04-2cbf-4a81-9afc-cb312ebb4dfe"
-
 #### 1. CONFIG
 curl -X POST http://localhost:8000/v1/config \
 -H "Content-Type: application/json" \
@@ -17,28 +14,25 @@ curl -X POST http://localhost:8000/v1/config \
 
 curl -X POST http://localhost:8000/v1/dataset/get-benchmark-dataset \
 -H "Content-Type: application/json" \
--d "{
-    \"session_id\": \"${SESSION_ID}\",
-    \"user_id\" : \"jjy714\",
-    \"dataset_name\": \"response_merged_output.csv\"
-}"
+-d '{
+    "session_id":"bf6a7de5-4007-49db-9d02-24e544896f8f",
+    "user_id" : "jjy714",
+    "dataset_name": "response_merged_output.csv"
+}'
 
 #### 3. EVALUATE
 
 curl -X POST http://localhost:8000/v1/evaluate/ \
 -H "Content-Type: application/json" \
--d "{
-    \"session_id\": \"${SESSION_ID}\",
-    \"user_id\": \"jjy714\"
-}"
+-d '{
+    "session_id": "70194e51-4166-43e3-ab22-af10faf9163b",
+    "user_id": "jjy714"
+}'
 
 
 
 ### 0. Insert Data
 
 curl -X POST \
-    -F "file=@/Users/jason/Claion/RAG/RAG_Evaluation/RAG_Evaluation/data/response_merged_output.csv" \
+    -F "file=@/home/minjichoi/RAG-Evaluation/RAG_Evaluation/data/response_merged_output.csv" \
     "http://localhost:8001/v1/insert?user_id=jjy714"
-
-
-    
